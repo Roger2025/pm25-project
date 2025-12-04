245df23d4b5c8ac0471c8e9d5bd245668dda31ca
roger2022 <A127038349@gmail.com>
2025-12-04 17:00:54 +0800

    完成書籍列表

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
+
 
 @app.route("/")
 def index():
-    return "<h1>Hello Flask!!!!</h1>"
+    time = now_time()
+    return render_template("index.html", time=time, name="Roger2022")
 
 
 @app.route("/nowtime")
-def nowtime():
+def now_time():
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(time)
     return time
@@ -27,12 +46,10 @@ def get_bmi(h, w):
 @app.route("/books/id=<int:id>")
 def get_books(id=None):
     try:
-        books = {1: "Python book", 2: "Java book", 3: "Flask book"}
-
         if id == None:
-            return books
+            return render_template("books.html", books=books)
 
-        return books[id]
+        return render_template("books.html", books=books[id])
     except Exception as e:
         return f"書籍編號錯誤:{e}"
 
